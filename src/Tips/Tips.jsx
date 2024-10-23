25653cc7fe6aaad7beb51c4960ae99205bbc7511
haseeb-developer <haseeb.developer91@gmail.com>
2024-10-23 23:56:30 +0500

    Updated

--- src/Tips/Tips.jsx
+++ src/Tips/Tips.jsx
@@ -7,18 +7,18 @@ const Tips = () => {
       <h2 className="tips-title">Key Points</h2>
       <ul className="tips-list">
         <li className="tip-item">
-          No underscores at the start or end of the username; use them only in
-          the middle.
-        </li>
-        <li className="tip-item">Offensive usernames are not allowed.</li>
-        <li className="tip-item">
-          Numbers before '@' in email field are not allowed.
-        </li>
-        <li className="tip-item">Special character are not allowed.</li>
-        <li className="tip-item">
           If your domain (From Select Box) not matching with the input, It won't
           let you proceed.
         </li>
+        <li className="tip-item">
+          No underscores at the start or end of the username; use them only in
+          the middle.
+        </li>
+        <li className="tip-item">
+          Numbers before '@' in email field are not allowed.
+        </li>
+        <li className="tip-item">Offensive usernames are not allowed.</li>
+        <li className="tip-item">Special character are not allowed.</li>
       </ul>
     </div>
   );
