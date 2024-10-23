3f0d5accd248f30e3ab4335f0495b6baf9f51dd4
haseeb-developer <haseeb.developer91@gmail.com>
2024-10-23 23:14:04 +0500

    Updated

--- src/Tips/Tips.jsx
+++ src/Tips/Tips.jsx
@@ -4,7 +4,7 @@ import "./Tips.css";
 const Tips = () => {
   return (
     <div className="tips-container">
-      <h2 className="tips-title">Username Guidelines</h2>
+      <h2 className="tips-title">Key Points</h2>
       <ul className="tips-list">
         <li className="tip-item">
           No underscores at the start or end of the username; use them only in
@@ -12,6 +12,9 @@ const Tips = () => {
         </li>
         <li className="tip-item">Offensive usernames are not allowed.</li>
         <li className="tip-item">Special character are not allowed.</li>
+        <li className="tip-item">
+          If your domain not matching with the input, It won't let you proceed.
+        </li>
       </ul>
     </div>
   );
